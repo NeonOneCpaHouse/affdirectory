4fac0d3c8d6abe897cdccd0491c611f669e32842
NeonOne <neonone@172-2-2-45.lightspeed.dybhfl.sbcglobal.net>
2026-04-01 19:38:08 +0300

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
+import { Analytics } from "@vercel/analytics/next"
 import { Inter } from "next/font/google"
 import Script from "next/script"
 import "./globals.css"
@@ -61,6 +62,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ThemeProvider>
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
